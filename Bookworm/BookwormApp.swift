44d9ac62dbf433ddaf40dab2e82272cd148928ea
Miguel Quezada <mig.aquezada@gmail.com>
2022-06-04 20:28:16 -0400

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Miguel Quezada on 04-06-22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
